01d6ab2f011f049986eae9124200a50206852ef8
PreslavKozovski <preslav.kozovski@gmail.com>
2021-02-04 18:17:28 +0200

    chore: update playground

--- playground/button.js
+++ playground/button.js
@@ -4,10 +4,11 @@ const fs = require('fs');
 const path = require('path');
 
 
-const file = fs.readFileSync(path.join(__dirname, 'data/button.scss'), 'utf8');
+const file = fs.readFileSync(path.join(__dirname, 'data/vars.scss'), 'utf8');
 
 dss.parse(file, {}, function(parsed) {
     const data = parsed.blocks[0];
+    debugger
 
     console.log(data);
 });
