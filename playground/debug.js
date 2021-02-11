1c987be8fe629f2fb8c8ed158dd50bb5a1ab1836
PreslavKozovski <preslav.kozovski@gmail.com>
2021-02-11 15:05:29 +0200

    chore: add vscode launch.json for easier debugging

--- playground/debug.js
+++ playground/debug.js
@@ -3,10 +3,11 @@ const dss = require('../dss');
 const fs = require('fs');
 const path = require('path');
 
-
 const file = fs.readFileSync(path.join(__dirname, 'data/all-annotations.scss'), 'utf8');
+//const file = fs.readFileSync(path.join(__dirname, 'data/button.scss'), 'utf8');
+//const file = fs.readFileSync(path.join(__dirname, 'data/vars.scss'), 'utf8');
 
-dss.parse(file, {}, function(parsed) {
+dss.parse(file, {}, (parsed) => {
     const data = parsed.blocks[0];
     debugger
 
